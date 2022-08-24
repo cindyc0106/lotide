b6e3fb5b35812a319cbd9f141cd3130835bb973c
Cindy <cindy.c0106@gmail.com>
2022-08-24 20:48:18 +0000

    adding countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//new project below
+
+const countLetters = function(string) {
+  let result = {};
+  for (const letter of string) {
+    if (letter !== " ") {
+      if (result[letter]) {
+        result[letter] += 1;
+      } else {
+        result[letter] = 1;
+      }
+    }
+    
+  }
+
+  return result;
+
+};
+console.log(countLetters("cindy chen"))
