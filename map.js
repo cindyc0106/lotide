3eccd0fca73e6b1ef80126de830d696b6231b3a6
Cindy <cindy.c0106@gmail.com>
2022-08-25 21:13:45 +0000

    map.js

---
+++ map.js
@@ -0,0 +1,36 @@
+function eqArrays(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false
+  } else {
+   for(let i = 0; i < array1.length; i++) {
+    if(array1[i] !== array2[i]) {
+      return false
+    } 
+   }
+  } return true
+}
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅  Arrays are equal: ${array1}  === ${array2}`);
+  } else {
+    console.log(`🔴🔴🔴  Arrays are not equal: ${array1} !== ${array2}`);
+  }
+};
+
+//Assignment below
+
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+//const results1 = map(words, word => word[0]);
+//console.log(results1);
+//console.log(assertArraysEqual(results1, ["g", "c", "t", "m", "t"])); 
+
+
+
