9412075f91b43378c612219faee58f3ca99396ec
Cindy <cindy.c0106@gmail.com>
2022-08-29 22:56:38 +0000

    refactor with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,21 @@
-const assertEqual = require('../assertEqual');
-const eqArrays = require('../eqArrays');
-const assertArraysEqual = require('../assertArraysEqual');
+//const assertEqual = require('../assertEqual');
+//const eqArrays = require('../eqArrays');
+//const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle')
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3] )
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+
+
+//assertArraysEqual(middle([1, 2, 3, 4]), [2, 3] )
+//assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+
+describe("#middle", () => {
+  it("should get back 2, 3", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("should get back 3, 4", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+});
